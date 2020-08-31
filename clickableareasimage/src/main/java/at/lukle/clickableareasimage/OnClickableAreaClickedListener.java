d33242ce8fd18ad8008b1c1be5a18c6a81a49805
Abhishek Ghatge <36225932+abhi8422@users.noreply.github.com>
2020-08-31 16:01:14 +0530

    Update OnClickableAreaClickedListener.java

--- clickableareasimage/src/main/java/at/lukle/clickableareasimage/OnClickableAreaClickedListener.java
+++ clickableareasimage/src/main/java/at/lukle/clickableareasimage/OnClickableAreaClickedListener.java
@@ -3,6 +3,6 @@ package at.lukle.clickableareasimage;
 /**
  * Created by Lukas on 10/22/2015.
  */
-public interface OnClickableAreaClickedListener<T> {
+public interface OnClickableAreaClickedListener<T,View> {
     void onClickableAreaTouched(T item,View view);
 }
